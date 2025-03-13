552827d7bbb1dddd57fc5342e2f6c09930652be6
OGKodingKat <katmarie9628@gmail.com>
2025-03-13 12:15:08 -0500

    commit changes to all files

--- 2-code-along/index.js
+++ 2-code-along/index.js
@@ -1,1 +1,14 @@
 // Here is where you will write your bill splitter code
+// this is dynamic or user generate
+let bill = Number(process.argv[2]);
+let tipPercentage = Number(process.argv[3]) / 100;
+let numGuests = Number(process.argv[4]);
+
+
+//Business Logic
+let tipAmount = bill * tipPercentage;
+let total = bill + tipAmount;
+let amountOwedPerGuest = total / numGuests;
+
+console.log(`Each guest owes $${amountOwedPerGuest}`);
+// console.log(process.argv);
--- 3-dog-age-calculator/index.js
+++ 3-dog-age-calculator/index.js
@@ -1,1 +1,22 @@
 // Your code will go here!
+
+let dogName = process.argv[2];
+let dogAgeInHumanYears = Number(process.argv[3]);
+
+//Business Logic
+let dogFirstYear = 15;
+let dogSecondYear = 9;
+let dogRemainingYears = 5;
+// let totalDogYears = dogFirstYear + dogSecondYear + (dogRemainingYears * (dogAgeInHumanYears - 2));
+let dogTotalYears;
+if (dogAgeInHumanYears === 1) {
+  dogTotalYears = dogFirstYear;
+} else if (dogAgeInHumanYears === 2) {
+  dogTotalYears = dogFirstYear + dogSecondYear;
+} else {
+  dogTotalYears =
+    dogFirstYear + dogSecondYear + dogRemainingYears * (dogAgeInHumanYears - 2);
+}
+
+//RESPONSE: Send the response back to the frontend so that it can be displayed to the user
+console.log(`Your dog ${dogName} is ${dogTotalYears} years old in dog years.`);

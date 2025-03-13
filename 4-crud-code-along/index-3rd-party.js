7ec4727b19bdff09a85a3c346d9c3002772e6aa8
OGKodingKat <katmarie9628@gmail.com>
2025-03-13 12:31:52 -0500

    commit 3rd party

--- 4-crud-code-along/index-3rd-party.js
+++ 4-crud-code-along/index-3rd-party.js
@@ -1,1 +1,10 @@
 // Create a program that checks to see if the current year is leap year using the Moment module.
+import moment from 'moment';
+
+let isCurrentYearLeap = moment().format();
+
+if (isCurrentYearLeap) {
+  console.log('Current year is a leap year');
+} else {
+  console.log('Current year is not a leap year');
+}
